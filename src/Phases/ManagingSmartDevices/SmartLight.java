0591310b92c9af858ba4c93a4eaf31fa51545cde
GitaMansouri <gita.mansouri.84@gmail.com>
2025-04-24 16:23:50 -0700

    build SmartLight class

---
+++ src/Phases/ManagingSmartDevices/SmartLight.java
@@ -0,0 +1,45 @@
+package src.Phases.ManagingSmartDevices;
+
+public class SmartLight extends SmartDevice {
+    private int brightness;
+
+    public SmartLight(String name, String protocol) {
+        super(name, protocol);
+        this.brightness = 50;
+    }
+
+    @Override
+    protected void initializeProperties() {
+        addProperty("status", String.class, "on", "off");
+        addProperty("brightness", Integer.class, 0, 100);
+    }
+
+    @Override
+    public DeviceType getType() {
+        return DeviceType.LIGHT;
+    }
+
+    @Override
+    protected boolean setStringProperty(String property, String value) {
+        if ("status".equals(property)) {
+            setStatus(value);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected boolean setNumericProperty(String property, int value) {
+        if ("brightness".equals(property)) {
+            brightness = value;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String getDeviceInfo() {
+        return String.format("Light: %s | Status: %s | Brightness: %d%% | Protocol: %s",
+                getName(), getStatus(), brightness, getProtocol());
+    }
+}
